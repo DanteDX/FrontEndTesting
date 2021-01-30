eac4a0ce7d982344eadbf264fb1cf29b23bc2d10
dantedxwolf <teacher.martin@gmail.com>
2021-01-30 22:14:56 +0600

    jest version problem fixed

---
+++ src/App.test.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('First set of tests', () => {
+  test('First First Test', () => {
+    expect(1).toBe(1)
+  })
+
+  test('First Second Test', () => {
+    expect(2).toBe(2)
+  });
+
+  test('First Third Test', () => {
+    expect(3).toBe(3)
+  })
+})
